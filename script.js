a8cbf903d9960d8c7b24eaefe4ed034d84d88c5e
charanjit <rs970453@gmail.com>
2023-08-28 20:42:47 +0530

    stable all functions working

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
 const todo = document.getElementById("toinput");
-const list = document.getElementById("todo-list");
-function addtask(){
-    if(todo.value ===''){
-        alert("you must write something!");
+const btn = document.getElementById("inbtn");
+const btnchange = document.getElementById("inbtn").innerHTML;
+const data = document.getElementById("rec");
+let userArray = [];
+let edit_id = null;
+
+let obj = localStorage.getItem('user');
+if(obj!= null){
+    userArray = JSON.parse(obj);
+}
+
+displayInfo();
+
+btn.onclick=()=>{
+    const name = todo.value;
+    if(edit_id!=null){
+        //edit
+        userArray.splice(edit_id, 1, {'name': name});
+        edit_id = null;
     } else {
-        let li = document.createElement('li');
-        li.textContent = todo.value;
-        list.appendChild(li);
+        // insert
+        userArray.push({'name': name});
     }
+   saveInfo(userArray);
+   todo.value = '';
+   btn.innerHTML = btnchange;
+}
+
+function saveInfo(ary){
+    let str = JSON.stringify(userArray);
+    localStorage.setItem('user',str);
+    displayInfo();
+}
+
+function displayInfo(){
+    let state = '';
+    userArray.forEach((user,i) =>{
+        state += `<tr>
+        <th scope="row">${i+1}</th>
+        <td style="width: 60%">${user.name}</td>
+        <td><i class="btn btn-success fa-regular fa-pen-to-square" onclick="editInfo(${i})"></i> <i class="btn btn-success fa-solid fa-trash" onclick="deleteInfo(${i})"></i></td>
+      </tr>`;
+    });
+    data.innerHTML = state;
+}
+
+function editInfo(id){    
+    edit_id = id;
+    todo.value = userArray[id].name;
+    btn.innerHTML = '<i class="fa-solid fa-floppy-disk"></i>';
+}
+
+function deleteInfo(id){
+    userArray.splice(id, 1);
+    saveInfo(userArray);
 }
